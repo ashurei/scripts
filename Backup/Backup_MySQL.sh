08f07c35ab5600bbb10ffa5f4f838ec6044f5531
ashurei <anjae83@gmail.com>
2020-08-31 11:07:25 +0900

    mysqldump 옵션 추가 등.
    mysqldump 옵션 추가, tee 로깅 변경, mysql 접속 체크, port 제거.

--- Backup/Backup_MySQL.sh
+++ Backup/Backup_MySQL.sh
@@ -2,7 +2,7 @@
 #################################################
 # Description : MySQL/MariaDB mysqldump
 # Create DATE : 2020.03.11
-# Last Update DATE : 2020.08.21 by ashurei
+# Last Update DATE : 2020.08.31 by ashurei
 # Copyright (c) Technical Solution, 2020
 #################################################
 
@@ -10,60 +10,38 @@
 MYSQL_ID="root"
 ################################
 # Need to modify
-SERVICE="DNS"
-MYSQL_PW="password"
-PORT="3307"
-SOCKET="/tmp/mysql.sock"
-BACKDIR="/MYSQL/BACKUP/mysqldump"
+SERVICE="test"
+MYSQL_PW="imsi00"
+SOCKET="/tmp/mysql-test.sock"
+BACKDIR="/home/mysql/mysqldump"
 ################################
 DATE=$(date '+%Y%m%d')
 BACKLOG=${BACKDIR}/mysql_mysqldump_${DATE}.log
 TARGET=${BACKDIR}/${DATE}
 DIV="#############################################################################################"
 
-### Check DB process
-IS_EXIST_DB=$(ps aux | grep "mysqld" | grep -v "mysqld_safe" | grep -v grep | wc -l)
-if [ "${IS_EXIST_DB}" -lt 1 ]
-then
-	echo "[DB BACKUP] There is not DB process."
-	exit 1
-fi
-
-### Create directory
-if [ ! -d "${TARGET}/dump" ] || [ ! -d "${TARGET}/log" ] || [ ! -d "${TARGET}/conf" ]
-then
-	mkdir -p "${TARGET}"/dump
-	mkdir -p "${TARGET}"/log
-	mkdir -p "${TARGET}"/conf
-fi
-
 
 #==============================================================================================================#
-### Backup config files
-cp /etc/my.cnf "${TARGET}"/conf/my.cnf_"${DATE}" >> "${BACKLOG}" 2>&1
-
-
-#==============================================================================================================#
-### Get mysql directory
-# MySQL or MariaDB yum install version
-if [ -f "/usr/bin/mysql" ]
+### Get mysql path
+# SKT standard
+if [ -f "/MYSQL/mysql/bin/mysql" ]
 then
-	MYSQL_DIR="/usr/bin"
+        MYSQL_DIR="/MYSQL/mysql/bin"
+# MySQL or MariaDB rpm client
+elif [ -f "/usr/bin/mysql" ]
+then
+        MYSQL_DIR="/usr/bin"
 # MySQL or MariaDB binary install version
 elif [ -f "/usr/local/mysql/bin/mysql" ]
 then
-	MYSQL_DIR="/usr/local/mysql/bin"
-# SKT standard
-elif [ -f "/MYSQL/mysql/bin/mysql" ]
-then
-	MYSQL_DIR="/MYSQL/mysql/bin"
+        MYSQL_DIR="/usr/local/mysql/bin"
 # Find with 'which'
 elif [ "$(which mysql 2>dev/null)" ]
 then
-	MYSQL_DIR=$(which mysql)
+        MYSQL_DIR=$(which mysql)
 else
-	echo "[${DATE}] (ERROR) Not setted MySQL execute path." >> "${BACKLOG}"
-	exit 1
+        echo "[${DATE}] (ERROR) Not setted MySQL execute path." | tee -a "${BACKLOG}"
+        exit 1
 fi
 
 MYSQL=${MYSQL_DIR}/mysql
@@ -74,18 +52,47 @@ MYSQLDUMP=${MYSQL_DIR}/mysqldump
 ### Get value from Database
 function getValue()
 {
-	VALUE=$(${MYSQL} --user="${MYSQL_ID}" --password="${MYSQL_PW}" --port=${PORT} --socket=${SOCKET} \
-		--skip-column-names \
-		--silent \
-		--execute="$1")
-	echo "${VALUE}"
+        VALUE=$(${MYSQL} --user="${MYSQL_ID}" --password="${MYSQL_PW}" --socket=${SOCKET} \
+                        --skip-column-names --silent --execute="$1")
+        echo "${VALUE}"
 }
 
 
 #==============================================================================================================#
-### Get 'log_bin' option
-LOG_BIN=$(getValue "show variables like 'log_bin'")
-LOG_BIN=$(echo "${LOG_BIN}" | awk '{print $2}')
+### Prepare Backup process
+# Check DB process
+IS_EXIST_DB=$(ps aux | grep "mysqld" | grep -v "mysqld_safe" | grep -v grep | wc -l)
+if [ "${IS_EXIST_DB}" -lt 1 ]
+then
+        echo "[${DATE} $(date '+%H:%M:%S')] There is not DB process." | tee -a "${BACKLOG}"
+        exit 1
+fi
+
+# Check connection to mysql server
+IS_CONN=$(getValue "select 1")
+if [ "${IS_CONN}" != 1 ]
+then
+        echo "[${DATE} $(date '+%H:%M:%S')] You cannot connect mysql server." | tee -a "${BACKLOG}"
+        exit 1
+fi
+
+# Check exist backup
+if [ -d "${TARGET}" ]
+then
+        echo "[${DATE} $(date '+%H:%M:%S')] There is already DB Backup today." | tee -a "${BACKLOG}"
+        exit 1
+fi
+
+# Create directory
+if [ ! -d "${TARGET}/dump" ] || [ ! -d "${TARGET}/log" ] || [ ! -d "${TARGET}/conf" ]
+then
+        mkdir -p "${TARGET}"/{dump,log,conf}
+fi
+
+
+#==============================================================================================================#
+### Backup config files
+cp /etc/my.cnf "${TARGET}"/conf/my.cnf_"${DATE}" >> "${BACKLOG}" 2>&1
 
 
 #==============================================================================================================#
@@ -94,10 +101,12 @@ DATABASE=$(getValue "show databases")
 
 
 #==============================================================================================================#
-### Set 'MASTER_DATA'
+### Get 'log_bin' option and set '--master-data'
+LOG_BIN=$(getValue "show variables where variable_name='log_bin'")
+LOG_BIN=$(echo "${LOG_BIN}" | awk '{print $2}')
 if [ "${LOG_BIN}" = "ON" ]
 then
-	MASTER="--master-data=2"
+        MASTER="--master-data=2"
 fi
 
 
@@ -106,9 +115,9 @@ fi
 {
 echo "[${DATE} $(date '+%H:%M:%S')] Delete backup files"
 # Delete backup file 1 days+ ago
-find ${BACKDIR:?} -mmin +1440 -type d -regextype posix-extended -regex "./[0-9]{8}" -print0 | xargs -0 rm -r
+find ${BACKDIR:?} -mmin +1440 -type d -regextype posix-extended -regex "${BACKDIR:?}/[0-9]{8}" -print0 | xargs -0 rm -r
 # Delete log file 7 day+ ago
-find ${BACKDIR:?}/*_backup_*.log -mtime +6 -type f -delete
+find ${BACKDIR:?}/mysql_mysqldump_*.log -mtime +6 -type f -delete
 } >> "${BACKLOG}" 2>&1
 
 
@@ -118,25 +127,27 @@ TIME_S=$(date '+%s')
 echo "[${DATE} $(date '+%H:%M:%S')] Backup start." >> "${BACKLOG}"
 for db in ${DATABASE}
 do
-	TIME_1=$(date '+%s')
-	OUTPUT="${TARGET}/dump/${db}_${DATE}.sql"
-	LOG="${TARGET}/log/${db}_${DATE}.log"
-	${MYSQLDUMP} --user=${MYSQL_ID} --password=${MYSQL_PW} -v ${MASTER} --single-transaction "${db}" 2>"${LOG}" > "${OUTPUT}"
+        TIME_1=$(date '+%s')
+        OUTPUT="${TARGET}/dump/${db}_${DATE}.sql"
+        LOG="${TARGET}/log/${db}_${DATE}.log"
+        ${MYSQLDUMP} --user=${MYSQL_ID} --password=${MYSQL_PW} --socket=${SOCKET} -v \
+                        ${MASTER} --single-transaction --events --routines --triggers "${db}" 2>"${LOG}" > "${OUTPUT}"
 
-	TIME_2=$(date '+%s')
-	ELASPED_TIME=$(( TIME_2 - TIME_1 ))
+        TIME_2=$(date '+%s')
+        ELASPED_TIME=$(( TIME_2 - TIME_1 ))
 
-	echo >> "${LOG}"
-	echo "Elapse time : ${ELASPED_TIME} sec" >> "${LOG}"
+        echo >> "${LOG}"
+        echo "Elapse time : ${ELASPED_TIME} sec" >> "${LOG}"
 done
 
 
 #==============================================================================================================#
 ### tar backup files
+# Do not use absolute path when perform 'tar' for security problem
 cd "${BACKDIR}" >> "${BACKLOG}" 2>&1 || exit
 {
 echo "[${DATE} $(date '+%H:%M:%S')] Compress"
-tar cvfzp "${TARGET}"/mysqldump_"${SERVICE}"_"${DATE}".tar.gz "${DATE}"
+tar cvfzp "${TARGET}"/mysqldump_"${SERVICE}"_"${DATE}".tar.gz "${DATE}"/{dump,log,conf}
 } >> "${BACKLOG}" 2>&1
 
 
