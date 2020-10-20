c2a5368f79eb2ddc371adcec91e7dde23af5ce52
ashurei <anjae83@gmail.com>
2020-10-20 14:45:48 +0900

    Update finding DEVICE
    ls -l /dev/* 구문에 awk 추가
    grep -w 옵션 추가

--- Oracle/get_asm_physical.sh
+++ Oracle/get_asm_physical.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#################################################
+########################################################
 # Description : Oracle ASM disk with physical disk
 # Create DATE : 2020.09.29
-# Last Update DATE : 2020.09.29 by ashurei
+# Last Update DATE : 2020.10.20 by ashurei
 # Copyright (c) Technical Solution, 2020
-#################################################
+########################################################
 
 # Insert to array per line
 IFS=$'\n' DISKS=$(ls -l /dev/oracleasm/disks/* | awk '{print $5,$6,$10}')
@@ -15,7 +15,7 @@ do
         A=$(echo ${list} | awk '{print $1}')
         B=$(echo ${list} | awk '{print $2}')
         DISK=$(echo ${list} | awk '{print $3}' | awk -F"/" '{print $5}')
-        DEVICE=$(ls -l /dev/* | grep ${A} | grep ${B} | awk '{print $10}')
+        DEVICE=$(ls -l /dev/* | awk '{print $5,$6,$10}' | grep -w ${A} | grep -w ${B} | awk '{print $3}')
 
         # Print oracleasm disk and block device (ex. DATA01 /dev/sdb1)
         echo -e "${DISK}\t${DEVICE}"
