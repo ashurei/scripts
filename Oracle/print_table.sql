7b2021a92f0a8eddf5f9c25c5a4ecba74be1af95
ashurei <anjae83@gmail.com>
2020-11-03 22:44:34 +0900

    Update print_table.sql

--- Oracle/print_table.sql
+++ Oracle/print_table.sql
@@ -7,30 +7,24 @@ is
     l_descTbl       dbms_sql.desc_tab;
     l_colCnt        number;
 begin
-      dbms_sql.parse(  l_theCursor,  p_query, dbms_sql.native );
-    dbms_sql.describe_columns
-    ( l_theCursor, l_colCnt, l_descTbl );
+    dbms_sql.parse(l_theCursor, p_query, dbms_sql.native);
+    dbms_sql.describe_columns(l_theCursor, l_colCnt, l_descTbl);
 
     for i in 1 .. l_colCnt loop
-        dbms_sql.define_column
-        (l_theCursor, i, l_columnValue, 4000);
+        dbms_sql.define_column(l_theCursor, i, l_columnValue, 4000);
     end loop;
 
     l_status := dbms_sql.execute(l_theCursor);
 
-    while ( dbms_sql.fetch_rows(l_theCursor) > 0 ) loop
+    while (dbms_sql.fetch_rows(l_theCursor) > 0) loop
         for i in 1 .. l_colCnt loop
-            dbms_sql.column_value
-            ( l_theCursor, i, l_columnValue );
-            dbms_output.put_line
-            ( rpad( l_descTbl(i).col_name, 30 )
-              || ': ' ||
-              l_columnValue );
+            dbms_sql.column_value(l_theCursor, i, l_columnValue);
+            dbms_output.put_line( rpad( l_descTbl(i).col_name, 30 ) || ': ' || l_columnValue);
         end loop;
         dbms_output.put_line( '-----------------' );
     end loop;
 exception
     when others then
-     raise;
+        raise;
 end;
 /
