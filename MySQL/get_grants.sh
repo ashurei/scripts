9ef33c1c1e8e1d42896a2c3bd17b2b1782a8c291
ashurei <anjae83@gmail.com>
2020-11-16 16:09:11 +0900

    Renewal get_grants.sql
    socket 입력 부분 보완.
    getValue() 함수 추가

--- MySQL/get_grants.sh
+++ MySQL/get_grants.sh
@@ -1,68 +1,94 @@
 #!/bin/bash
 ##############################################################
 # Description : [MySQL] Get grants for mysqldump recovery
-# Create DATE : 2020.09.03
-# Last Update DATE : 2020.08.27 by ashurei
+# Create DATE : 2020.08.27
+# Last Update DATE : 2020.11.16 by ashurei
 # Copyright (c) Technical Solution, 2020
 ##############################################################
-usage ()
+
+#==============================================================================================================#
+### usage
+function usage ()
 {
-        echo "Usage: ./get_grant.sql [-u <db username> -p <password> -o <output file> [-s <socket>] ]"
+        echo "Usage: ./get_grant.sql [-u <db username> -p <password> -o <output file> [-S <socket>] ]"
 }
+
+### Get value from Database
+function getValue()
+{
+        VALUE=$(mysql --user="${MYSQL_ID}" --password="${MYSQL_PW}" ${MYSQL_SOCK} \
+                        --skip-column-names --silent --execute="$1")
+        echo "${VALUE}"
+}
+#==============================================================================================================#
+
 
 ### Read arguments
 while [ $# -gt 0 ]
 do
-        case "$1" in
-        -u)     MYSQL_USER=$2
-                shift
-                shift
-                ;;
-        -p)     MYSQL_PASSWD=$2
-                shift
-                shift
-                ;;
-        -o)     OUTPUT=$2
-                shift
-                shift
-                ;;
-        -s)     SOCKET=$2
-                shift
-                shift
-                ;;
-        -h|*)   usage
-                exit 1
-                ;;
-        esac
+    case "$1" in
+    -u) MYSQL_ID=$2
+        shift
+        shift
+        ;;
+    -p) MYSQL_PW=$2
+        shift
+        shift
+        ;;
+    -o) OUTPUT=$2
+        shift
+        shift
+        ;;
+    -S) SOCKET=$2
+        shift
+        shift
+        ;;
+    -h|*)  usage
+        exit 1
+        ;;
+    esac
 done
 
+### Create socket option
+if [ -n ${SOCKET} ]
+then
+    MYSQL_SOCK="--socket=${SOCKET}"
+fi
 
 ### Check necessary arguments
-if [[ -z ${MYSQL_USER} || -z ${MYSQL_PASSWD} || -z ${OUTPUT} ]]
+if [[ -z ${MYSQL_ID} || -z ${MYSQL_PW} || -z ${OUTPUT} ]]
 then
         usage
         exit 1
 fi
 
+### Check connection to mysql server
+IS_CONN=$(getValue "select 1")
+if [ "${IS_CONN}" != 1 ]
+then
+        echo "[${DATE} $(date '+%H:%M:%S')] You cannot connect mysql server."
+        exit 1
+fi
 
 ### Initialize OUTPUT file
 if [ ! -f "${OUTPUT}" ]
 then
-        cp /dev/null "${OUTPUT}"
+    cp /dev/null "${OUTPUT}"
 else
-        echo "OUTPUT file is exists."
-        exit 1
+    echo "OUTPUT file is exists."
+    exit 1
 fi
 
+#==============================================================================================================#
 ### Get user, host list
-USERS=$(mysql -u "${MYSQL_USER}" -p"${MYSQL_PASSWD}" -N -s -e "select concat('''',user,'''','@','''',host,'''') from user" mysql)
+USERS=$(getValue "select concat('''',user,'''','@','''',host,'''') from mysql.user")
 
 
 ### Create grant command to OUTPUT
 for user in ${USERS}
 do
-  GRANT_SQL="show grants for ${user}"
-  mysql -u "${MYSQL_USER}" -p"${MYSQL_PASSWD}" -N -s -e "${GRANT_SQL}" mysql >> "${OUTPUT}"
+    GRANT_SQL="show grants for ${user}"
+    getValue "${GRANT_SQL}" >> "${OUTPUT}"
 done
 
 
