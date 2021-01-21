7ddfef85a80a60a1d8bff1932ca60065193a7a6d
ashurei <anjae83@gmail.com>
2021-01-21 23:29:00 +0900

    pfile, *.ora file backup 수정
    pfile, *.ora file backup 수정

--- Backup/Backup_Oracle.sh
+++ Backup/Backup_Oracle.sh
@@ -2,18 +2,18 @@
 #################################################
 # Description : Oracle expdp
 # Create DATE : 2019.12.11
-# Last Update DATE : 2021.01.15 by ashurei
+# Last Update DATE : 2021.01.21 by ashurei
 # Copyright (c) Technical Solution, 2020
 #################################################
 
 ### Set variable
 ################################
 # Need to modify
-export ORACLE_SID="APCS01"
+export ORACLE_SID="UAPDB1"
 export ORACLE_HOME="/oracle/database/product/19"
 BACKDIR="/oracle/backup/expdp"
 USER="system"
-PASSWD="passwd"
+PASSWD="SKTelecom!2#4"
 ################################
 DATE=$(date '+%Y%m%d')
 BACKLOG=${BACKDIR}/oracle_expdp_${DATE}.log
@@ -25,33 +25,32 @@ OUTPUT="expdp_${ORACLE_SID}_${DATE}"
 IS_EXIST_DB=$(ps aux | grep "ora_pmon_${ORACLE_SID}" | grep -v grep | wc -l)
 if [ "${IS_EXIST_DB}" -lt 1 ]
 then
-	echo "[DB BACKUP] There is not DB process."
-	exit 1
+        echo "[DB BACKUP] There is not DB process."
+        exit 1
 fi
 
 ### Create directorys
 if [ ! -d "${BACKDIR}/dump" ] || [ ! -d "${BACKDIR}/log" ] || [ ! -d "${BACKDIR}/conf" ]
 then
-	mkdir -p "${BACKDIR}/{dump,log,conf}"
+        mkdir -p "${BACKDIR}/{dump,log,conf}"
 fi
 
 
 #==============================================================================================================#
 ### Backup config files
-### Backup config files
-sqlplus / as sysdba << EOF
-create pfile='${BACKDIR}/conf/init${ORACLE_SID}.ora_${DATE}' from spfile;
-EOF
+GRID_HOME="/oracle/grid"
+{
+echo "[${DATE} $(date '+%H:%M:%S')] Backup pfile and network config"
+cp "${GRID_HOME}"/network/admin/*.ora "${BACKDIR}"/conf/
+sqlplus -S / as sysdba <<< "create pfile='${BACKDIR}/conf/init${ORACLE_SID}.ora_${DATE}' from spfile;"
 
 
 #==============================================================================================================#
 ### Delete backup files
-{
 echo "[${DATE} $(date '+%H:%M:%S')] Delete backup files"
-# Delete backup file 1 days+ ago
+# Delete dump and log file 7 day+ ago
 find ${BACKDIR:?}/dump/*.dmp -mmin +1440 -type f -delete
-# Delete log file 7 day+ ago
-find ${BACKDIR:?}/*_backup_*.log -mtime +6 -type f -delete
+find ${BACKDIR:?}/oracle_expdp_*.log -mtime +6 -type f -delete
 find ${BACKDIR:?}/conf/init${ORACLE_SID}.ora* -mtime +6 -type f -delete
 find ${BACKDIR:?}/log/*.log -mtime +6 -type f -delete
 } >> "${BACKLOG}" 2>&1
