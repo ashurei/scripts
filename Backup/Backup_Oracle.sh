d2a72d16ab0a74b1c99588ec9e0d8c6b474a3208
ashurei <anjae83@gmail.com>
2021-09-02 21:55:05 +0900

    mkdir, netconf
    mkdir, netconf

--- Backup/Backup_Oracle.sh
+++ Backup/Backup_Oracle.sh
@@ -2,7 +2,7 @@
 #################################################
 # Description : Oracle expdp
 # Create DATE : 2019.12.11
-# Last Update DATE : 2021.01.21 by ashurei
+# Last Update DATE : 2021.09.02 by ashurei
 # Copyright (c) Technical Solution, 2020
 #################################################
 
@@ -22,39 +22,43 @@ DIR_LOG="DIR_LOG"
 OUTPUT="expdp_${ORACLE_SID}_${DATE}"
 
 ### Check DB process
-IS_EXIST_DB=$(ps aux | grep "ora_pmon_${ORACLE_SID}" | grep -v grep | wc -l)
+IS_EXIST_DB=$(ps ax | grep "ora_pmon_${ORACLE_SID}" | grep -v grep | wc -l)
 if [ "${IS_EXIST_DB}" -lt 1 ]
 then
-        echo "[DB BACKUP] There is not DB process."
-        exit 1
+  echo "[DB BACKUP] There is not DB process."
+  exit 1
 fi
 
 ### Create directorys
 if [ ! -d "${BACKDIR}/dump" ] || [ ! -d "${BACKDIR}/log" ] || [ ! -d "${BACKDIR}/conf" ]
 then
-        mkdir -p "${BACKDIR}/{dump,log,conf}"
+  mkdir -p "${BACKDIR}"/{dump,log,conf}
 fi
-
 
 #==============================================================================================================#
 ### Backup config files
-GRID_HOME="/oracle/grid"
+GRID_HOME=$(ps ax | grep ocssd.bin | grep -v grep | awk '{print $5}' | awk -F'/bin' '{print $1}')
+if [ -n "${GRID_HOME}" ]
+then
+  NETCONF="${GRID_HOME}/network/admin"
+else
+  NETCONF="${ORACLE_HOME}/network/admin"
+fi
+
 {
-echo "[${DATE} $(date '+%H:%M:%S')] Backup pfile and network config"
-cp "${GRID_HOME}"/network/admin/*.ora "${BACKDIR}"/conf/
-$ORACLE_HOME/bin/sqlplus -S / as sysdba <<< "create pfile='${BACKDIR}/conf/init${ORACLE_SID}.ora_${DATE}' from spfile;"
-
-
-#==============================================================================================================#
-### Delete backup files
-echo "[${DATE} $(date '+%H:%M:%S')] Delete backup files"
-# Delete dump and log file 7 day+ ago
-find ${BACKDIR:?}/dump/*.dmp -mmin +1440 -type f -delete
-find ${BACKDIR:?}/oracle_expdp_*.log -mtime +6 -type f -delete
-find ${BACKDIR:?}/conf/init${ORACLE_SID}.ora* -mtime +6 -type f -delete
-find ${BACKDIR:?}/log/*.log -mtime +6 -type f -delete
+  echo "[${DATE} $(date '+%H:%M:%S')] Backup pfile and network config"
+  cp "${NETCONF}"/listener.ora "${BACKDIR}"/conf/listener.ora_"${ORACLE_SID}"_"${DATE}"
+  cp "${NETCONF}"/sqlnet.ora "${BACKDIR}"/conf/sqlnet.ora_"${ORACLE_SID}"_"${DATE}"
+  $ORACLE_HOME/bin/sqlplus -S / as sysdba <<< "create pfile='${BACKDIR}/conf/init${ORACLE_SID}.ora_${DATE}' from spfile;"
+  
+  ### Delete backup files
+  echo "[${DATE} $(date '+%H:%M:%S')] Delete backup files"
+  # Delete dump and log file 7 day+ ago
+  find ${BACKDIR:?}/dump/*.dmp -mmin +1440 -type f -delete
+  find ${BACKDIR:?}/oracle_expdp_*.log -mtime +6 -type f -delete
+  find ${BACKDIR:?}/conf/init${ORACLE_SID}.ora* -mtime +6 -type f -delete
+  find ${BACKDIR:?}/log/*.log -mtime +6 -type f -delete
 } >> "${BACKLOG}" 2>&1
-
 
 #==============================================================================================================#
 ### Execute expdp
