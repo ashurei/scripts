4dba15774cf633182385950c64a4a5b0dc8d1f1c
ashurei <anjae83@gmail.com>
2023-03-28 01:20:04 +0900

    Update rman_arch_ORCL.sh

--- Oracle/rman_arch_ORCL.sh
+++ Oracle/rman_arch_ORCL.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 ########################################################
-# Description : Rman Backup Script
+# Description : Rman Archive log Backup Script
 # Create DATE : 2023.02.06
-# Last Update DATE : 2023.02.07 by ashurei
+# Last Update DATE : 2023.03.28 by ashurei
 # Copyright (c) ashurei@sktelecom.com, 2023
 ########################################################
 
-###################################################
+DATE=$(date '+%Y%m%d_%H')
+################################
 # Need to modify
 export ORACLE_HOME="/oracle/database/product/19"
 export ORACLE_SID="ORCL2"
-###################################################
+LOGDIR="${HOME}/DBA/script/log"
+RMANLOG="${LOGDIR}/rman_arch_${ORACLE_SID}_${DATE}.log"
+BACKDIR="/rman/${ORACLE_SID}"
+################################
 
 export NLS_DATE_FORMAT="YYYY/MM/DD HH24:MI:SS"
-DATE=$(date '+%Y%m%d')
-RMANLOG="~/DBA/script/rman/rman_arch_${ORACLE_SID}_${DATE}.log"
-BACKDIR="/rman/${ORACLE_SID}"
 
 ### Create backup directory
-if [ ! -d "${BACKDIR}" ]
+if [ ! -d "${LOGDIR}" ]
 then
   set -e
-  mkdir "${BACKDIR}"
+  mkdir -p "${LOGDIR}"
   set +e
 fi
 
+if [ ! -d "${BACKDIR}" ]
+then
+  echo "Backup directory is not exists."
+fi
+
+### Delete log
+find ${LOGDIR:?}/rman_arch_"${ORACLE_SID}"_*.log -mtime +30 -type f -delete 2>&1
+
+### rman backup
 "${ORACLE_HOME}"/bin/rman target / > "${RMANLOG}" << EOF
 run {
   configure retention policy to recovery window of 3 days;
-
   crosscheck backup of archivelog all;
   backup tag='${ORACLE_SID}_ARCH' format '${BACKDIR}/ARCH_%d_%U_%T'
   as compressed backupset
