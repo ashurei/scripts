00cfedfaa061cc4959089ea54f1673a1f1d8ac52
ashurei <anjae83@gmail.com>
2024-05-22 17:10:50 +0900

    Update getInfo.py

--- python/getInfo.py
+++ python/getInfo.py
@@ -12,7 +12,7 @@ import argparse
 import csv
 
 parser = argparse.ArgumentParser(
-        prog="get_remote_ip",
+        prog="getInfo",
         description="Get remote ip from SIMS REPORT csv file.",
         epilog="ashurei@sk.com"
 )
@@ -20,9 +20,11 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-f", "--file", help="Input file name.", required=True)
 args = parser.parse_args()
 
+# Read csv file
 f = open(args.file, "r")
 reader = csv.DictReader(f)
 
+# Process information
 temp = ""
 list_IP = []
 d_remote = {}
@@ -36,9 +38,9 @@ for dict_row in reader:
     # Insert IP to LIST
     list_IP.append(dict_row['Remote 접속 IP'])
     # Insert LIST to Dictionary for Remote IP
-    d_remote[dict_row['관리장비명']] = list_IP
+    d_remote[dict_row['SIMS 연동 IP']] = list_IP
     # Dictionary for SIMS IP
-    d_sims[dict_row['관리장비명']] = dict_row['SIMS 연동 IP']
+    d_sims[dict_row['SIMS 연동 IP']] = dict_row['SIMS 연동 IP']
 
     # Save current row value for next loop
     temp = dict_row['관리장비명']
@@ -57,7 +59,8 @@ f.close()
 
 # Create SQL to find resourceid, resourcename in TDIM
 f = open("./output/resource.sql", "w")
+f.write("cp /dev/null /tmp/tcore.txt\n")
 for key,value in d_sims.items():
-    sql = "mysql -u tcore -pTcore12# -h tcore-private-vip -e \"select resourceid, resourcename from vw_rep_resource_bas where ip='" + value + "'\" -sN tcore_resource\n"
+    sql = "mysql -u tcore -pTcore12# -h tcore-private-vip -e \"select ip, resourceid, resourcename from vw_rep_resource_bas where ip='" + value + "'\" -sN tcore_resource >> /tmp/tcore.txt\n"
     f.write(sql)
 f.close()
