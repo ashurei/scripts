6146ce107138367c5e645775b43ac61d79150287
ashurei <anjae83@gmail.com>
2024-05-22 15:44:10 +0900

    Update and rename get_remote_ip.py to getInfo.py

---
+++ python/getInfo.py
@@ -0,0 +1,63 @@
+########################################################
+# Description : Get remote ip from SIMS REPORT
+# Create DATE : 2024.05.21
+# Last Update DATE : 2024.05.22 by ashurei
+# Copyright (c) Technical Solution, 2024
+#########################################################
+
+# Create remote_ip.txt file from SIMS REPORT csv file.
+# Create resource.sql  file from SIMS REPORT csv file.
+
+import argparse
+import csv
+
+parser = argparse.ArgumentParser(
+        prog="get_remote_ip",
+        description="Get remote ip from SIMS REPORT csv file.",
+        epilog="ashurei@sk.com"
+)
+
+parser.add_argument("-f", "--file", help="Input file name.", required=True)
+args = parser.parse_args()
+
+f = open(args.file, "r")
+reader = csv.DictReader(f)
+
+temp = ""
+list_IP = []
+d_remote = {}
+d_sims = {}
+
+for dict_row in reader:
+    # temp != Machine IP, Initialize LIST because next machine is started.
+    if temp != dict_row['관리장비명']:
+        list_IP = []
+
+    # Insert IP to LIST
+    list_IP.append(dict_row['Remote 접속 IP'])
+    # Insert LIST to Dictionary for Remote IP
+    d_remote[dict_row['관리장비명']] = list_IP
+    # Dictionary for SIMS IP
+    d_sims[dict_row['관리장비명']] = dict_row['SIMS 연동 IP']
+
+    # Save current row value for next loop
+    temp = dict_row['관리장비명']
+
+f.close()
+
+# Create TXT for remote ip
+f = open("./output/remote_ip.txt", "w")
+for key,value in d_remote.items():
+    remote_ip = ""
+    for v in value:
+        remote_ip = remote_ip + " " + v
+    remote_ip_bar = remote_ip.lstrip().replace(" ", " |")
+    f.write(key + "\t" + remote_ip_bar + "\n")
+f.close()
+
+# Create SQL to find resourceid, resourcename in TDIM
+f = open("./output/resource.sql", "w")
+for key,value in d_sims.items():
+    sql = "mysql -u tcore -pTcore12# -h tcore-private-vip -e \"select resourceid, resourcename from vw_rep_resource_bas where ip='" + value + "'\" -sN tcore_resource\n"
+    f.write(sql)
+f.close()
