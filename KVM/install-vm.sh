f911ca418efb2be985ec3193bd0e60321fb64042
ashurei <anjae83@gmail.com>
2024-06-18 10:44:18 +0900

    Update and rename install-vm.sh to KVM/install-vm.sh

--- KVM/install-vm.sh
+++ KVM/install-vm.sh
@@ -2,7 +2,7 @@
 #####################################################################
 # Description : Install KVM image
 # Create DATE : 2024.05.31
-# Last Update DATE : 2024.05.31 by ashurei
+# Last Update DATE : 2024.06.14 by ashurei
 # Copyright (c) ashurei@sk.com, 2024
 #####################################################################
 
@@ -11,22 +11,23 @@
 
 ### Default Variables
 IMGDIR="/kvm/img"
-CPU=16
-MEM=16
-SIZE=30
-BASE_IMG="/kvm/_base/Rocky-8-GenericCloud-Base-8.8-20230518.0.x86_64.qcow2"
+CPU=4
+MEM=4
+#SIZE=30
+#BASE_IMG="/kvm/_base/Rocky-8-GenericCloud-Base-8.8-20230518.0.x86_64.qcow2"
 #BASE_IMG="/kvm/_base/rhel-guest-image-7.2-20160302.0.x86_64.qcow2"
-OS_VARIANT="rhel7.9"
+#OS_VARIANT="rhel7.9"
 
 ### argument
 while [ $# -gt 0 ]
 do
   case "$1" in
-  -h) echo "usage) ./install_vm.sh -h [Domain name]"
+  -h) echo "usage) ./install_vm.sh -n [Domain name]"
       echo "                       -c [Core count]"
       echo "                       -m [RAM size(GB)]"
+      echo "                       -s [Disk size(GB)]"
       echo "                       -i [Base image file path]"
-      echo "                       -o [OS variant] (rhel7.9)"
+      echo "                       -o [OS variant] (ex. rhel7.9, ubuntu22.04)"
       exit 0
       ;;
   -n) HOST=$2
@@ -63,20 +64,42 @@ then
   echo "(ERROR) Need hostname."
   exit 1;
 fi
+if [ -z "${BASE_IMG}" ]
+then
+  echo "(ERROR) Need base image."
+  exit 1;
+fi
+if [ -z "${OS_VARIANT}" ]
+then
+  echo "(ERROR) Need os variant."
+  exit 1;
+fi
 
 QCOW="${IMGDIR}/${HOST}-vda.qcow2"
 RAW="${IMGDIR}/${HOST}-vda.raw"
 MEM=$((MEM*1024))
 
 ### Prepare image file
+echo "Process (1/4): Copy image file from base image."
 #qemu-img create -f qcow2 -b "${BASE_IMG}" "${QCOW}" "${SIZE}"G
 cp "${BASE_IMG}" "${QCOW}"
+
 ### Convert to raw
+echo "Process (2/4): Convert to raw from qcow2."
 qemu-img convert -f qcow2 -O raw "${QCOW}" "${RAW}"
-### Resize
-qemu-img resize "${RAW}" "${SIZE}"G
+#rm "${QCOW}"
+
+### Resize disk size
+echo "Process (3/4): Resize the disk size."
+if [ -n "${SIZE}" ]
+then
+  qemu-img resize "${RAW}" "${SIZE}"G
+else
+  echo "Pass."
+fi
 
 ### virt-install
+echo "Process (4/4): virt-install"
 virt-install --import \
  --name "${HOST}" \
  --vcpus "${CPU}" --cpuset=1-21,44-65 \
@@ -84,7 +107,8 @@ virt-install --import \
  --memorybacking hugepages=on \
  --numatune 0 \
  --disk "${RAW}",cache=none \
- --network network:default \
- --network network:internal \
+ --network bridge=br-bond1,model=virtio \
  --os-variant "${OS_VARIANT}" \
- --noautoconsole
+ --noautoconsole # --noreboot
+# --network network:default \
+# --network network:internal \
