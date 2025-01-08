414ed7e3f25fe204e5bb7c8eb1571b4aa6c17f3d
ashurei <anjae83@gmail.com>
2025-01-08 18:50:08 +0900

    Update telegraf_tbs.sh

--- Oracle/telegraf_tbs.sh
+++ Oracle/telegraf_tbs.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+export ORACLE_BASE=/oracle/database
+export ORACLE_HOME=${ORACLE_BASE}/product/19
+export ORACLE_SID=ORCL1
+SQL="/oracle/backup/telegraf_oracle.sql"
+
+${ORACLE_HOME}/bin/sqlplus -S mon/mon << EOF
 SET HEADING OFF
 SET FEEDBACK OFF
 SET TIME OFF TIMING OFF
@@ -7,15 +14,15 @@ SET TERM OFF
 
 SELECT 'tablespace_usage,tablespace=' || t.tn || ' usage_percent=' ||
        to_char( round((t.sizes - f.sizes) /t.sizes * 100,2), 'FM9990.99' ) pct
-FROM    ( SELECT tablespace_name tn,
+  FROM  ( SELECT tablespace_name tn,
                  sum(bytes)/1024/1024 Sizes
-          FROM   dba_data_files
-          GROUP  BY tablespace_name) t,
+            FROM   dba_data_files
+           GROUP BY tablespace_name) t,
         ( SELECT tablespace_name tn,
                  sum(bytes)/1024/1024 sizes
-          FROM   dba_free_space
-          GROUP BY tablespace_name) f
-WHERE t.tn = f.tn
-ORDER BY Pct desc
+            FROM dba_free_space
+           GROUP BY tablespace_name) f
+ WHERE t.tn = f.tn
+ ORDER BY Pct desc
 /
 EOF
