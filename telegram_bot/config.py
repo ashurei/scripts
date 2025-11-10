e660873b3f421a0e35cdae33765306b26b8e604f
ashurei <anjae83@gmail.com>
2025-11-10 18:14:01 +0900

    Create config.py

---
+++ telegram_bot/config.py
@@ -0,0 +1,11 @@
+TELEGRAM_BOT_TOKEN = "7985619895:AAGNz9-JOO6911cjtdPEZlGhki8Ge-FhEYo"
+LOGIN_URL = "https://www.splib.or.kr/spjlib/program/memberLoginProc.do"
+INDEX_URL = "https://www.splib.or.kr/spjlib/index.do"
+LIST_URL = "https://www.splib.or.kr/spjlib/program/mypage/loanStatusList.do"
+
+LOGIN_DATA = [
+          ('ashurei','********'),
+          ('yjh0628','********'),
+          ('asa0130','********'),
+          ('asw0613','********'),
+        ]
